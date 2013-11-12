2231fed4ad5a0f97ebe1cfccd781014a74f80bfb
234 <ert>
2013-11-12 17:45:53 +0100

    test

---
+++ src/testy.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class testy {
+
+	@Test
+	public void testA() {
+		assertEquals(7, 6);
+	}
+	
+	@Test
+	public void testB() {
+		assertEquals(3, 3);
+	}
+	
+	@Test
+	public void testC() {
+		assertEquals(3, 4);
+	}
+	
+}
